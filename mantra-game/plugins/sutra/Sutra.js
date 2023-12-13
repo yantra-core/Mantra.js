667511b479e9268cd9b329aaf9e7bd9f90385079
Marak <marak.squires@gmail.com>
2023-12-12 21:03:01 -0500

    Updates to new Sutra API

--- mantra-game/plugins/sutra/Sutra.js
+++ mantra-game/plugins/sutra/Sutra.js
@@ -1,18 +1,16 @@
-// import sutra from '../../../../sutra/index.js';
-import sutra from '@yantra-core/sutra';
+import { createSutra } from '../../../../sutra/index.js';
+//import { sutra } from '@yantra-core/sutra';
 // handles input controller events and relays them to the game logic
 class Sutra {
   static id = 'sutra';
   constructor() {
     this.id = Sutra.id;
-    console.log('sutrasutrasutra', sutra)
-    this.sutra = sutra;
     this.inputCache = {};
   }
 
   init(game) {
     this.game = game;
-    this.game.createSutra = sutra.createSutra;
+    this.game.createSutra = createSutra;
     this.game.setSutra = this.setSutra.bind(this);
     this.game.setRules = this.setSutra.bind(this);
     this.game.systemsManager.addSystem(this.id, this);
