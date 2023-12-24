94b4b0c96f06bcd70d8fca3cd4f8c703f412a4b5
Marak <marak.squires@gmail.com>
2023-12-23 22:43:41 -0500

    Adds `Tile` Plugin

---
+++ mantra-game/plugins/tile/Tile.js
@@ -0,0 +1,168 @@
+// WIP
+// Tile.js - Marak Squires 2023
+// Implements support for Tiled JSON maps via the Tiled JSON format
+// see: https://doc.mapeditor.org/en/stable/reference/json-map-format/
+class Tile {
+  static id = 'tile';
+  constructor(game) {
+    this.game = game; // Store the reference to the game logic
+    this.id = Tile.id;
+  }
+
+  init(game) {
+    this.game = game;
+
+    // console.log('Tile.init()');
+
+    // Test map for orthogonal tilemap
+    let testJSON = {
+      "compressionlevel": -1,
+      "height": 20,
+      "infinite": false,
+      "layers": [
+        {
+          "data": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+          "height": 20,
+          "id": 1,
+          "name": "Tile Layer 1",
+          "opacity": 1,
+          "type": "tilelayer",
+          "visible": true,
+          "width": 30,
+          "x": 0,
+          "y": 0
+        }],
+      "nextlayerid": 2,
+      "nextobjectid": 1,
+      "orientation": "orthogonal",
+      "renderorder": "right-down",
+      "tiledversion": "1.10.2",
+      "tileheight": 16,
+      "tilesets": [
+        {
+          "firstgid": 1,
+          "source": "grass-land.tsx"
+        }],
+      "tilewidth": 16,
+      "type": "map",
+      "version": "1.10",
+      "width": 30
+    }
+
+    let that = this;
+    setTimeout(function(){
+      that.createTileMapFromTiledJSON(testJSON);
+
+    }, 1000)
+  }
+
+  createTileMapFromTiledJSON(tiledJSON) {
+    // Assuming the JSON data is already parsed into a JavaScript object
+    const mapData = tiledJSON;
+
+    // Create a container for the map
+    const mapContainer = document.createElement('div');
+    mapContainer.style.position = 'relative';
+    mapContainer.style.width = mapData.width * mapData.tilewidth + 'px';
+    mapContainer.style.height = mapData.height * mapData.tileheight + 'px';
+
+    // Process each layer
+    mapData.layers.forEach(layer => {
+      // Only process if it's a tile layer
+      if (layer.type === 'tilelayer') {
+        this.createLayer(mapContainer, layer, mapData.tilewidth, mapData.tileheight);
+      }
+    });
+
+    // Append the map container to the body or a specific element
+    document.body.appendChild(mapContainer);
+  }
+
+  createLayer(container, layer, tileWidth, tileHeight) {
+    layer.data.forEach((tileId, index) => {
+      if (tileId !== 0) { // Assuming 0 is an empty tile
+        let scale = 1;
+        scale = 10;
+        // console.log('cccc', tileId, index)
+        
+        let x = (index % layer.width) * tileWidth;
+        let y = Math.floor(index / layer.width) * tileHeight;
+        let height = tileHeight;
+        let width = tileWidth;
+
+        // apply scale
+        x = x * scale;
+        y = y * scale;
+        height = height * scale;
+        width = width * scale;
+
+        console.log("placing at", x, y)
+        let ent = this.game.createEntity({
+          // id: 'tile' + index,
+          type: 'BLOCK',
+          body: false,
+          position: {
+            x: x,
+            y: y
+          },
+          color: 0x00ff00,
+          texture: 'img/game/tiles/' + tileId + '.png',
+          width: width,
+          height: height
+          // tileId: tileId
+        })
+        console.log("ent", ent)
+        /*
+        const tile = document.createElement('div');
+        tile.style.width = tileWidth + 'px';
+        tile.style.height = tileHeight + 'px';
+        tile.style.position = 'absolute';
+        tile.style.left = (index % layer.width) * tileWidth + 'px';
+        tile.style.top = Math.floor(index / layer.width) * tileHeight + 'px';
+
+        // Apply background image based on tileId - this needs a mapping from tileId to image
+        // For simplicity, let's assume a function getTileImageURL(tileId) that returns the image URL
+        tile.style.backgroundImage = `url('${this.getTileImageURL(tileId)}')`;
+
+        container.appendChild(tile);
+        */
+      }
+    });
+  }
+
+  getTileImageURL(tileId) {
+    // Placeholder function: implement mapping of tileId to actual image URLs
+    // For example, return 'path/to/image' + tileId + '.png';
+    return 'img/game/tiles/' + tileId + '.png';
+  }
+
+
+  update() {
+  }
+
+  render() { }
+
+  destroy() { }
+
+}
+
+export default Tile;
