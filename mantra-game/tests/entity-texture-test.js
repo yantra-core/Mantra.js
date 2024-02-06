eb2d2a70822d21a778eca16f64d61a6328f71d20
Marak <marak.squires@gmail.com>
2024-02-06 15:54:11 -0500

    Adds basic unit test for entity textures

--- mantra-game/tests/entity-texture-test.js
+++ mantra-game/tests/entity-texture-test.js
@@ -8,7 +8,6 @@ import Graphics from '../plugins/graphics/Graphics.js';
 
 let mockRoot = 'http://mock-url.com/';
 
-
 const game = new Game({
   loadDefaultPlugins: false,
   msgpack: false,
@@ -52,14 +51,9 @@ global.document = {
   }
 };
 
-tap.test('Basic Texture Tests', (t) => {
-  t.test('Can get null texture on empty entityData', (t) => {
-    let texture = game.getTexture({});
-    t.ok(texture === null, 'Texture should be null');
-    t.end();
-  });
+tap.test('Entity Texture Tests', (t) => {
 
-  t.test('Can get image sprite texture based on entityData.texture', async (t) => {
+  t.test('Preload the textures to use for entity', async (t) => {
 
     game.preloader.addAsset('mock-url-1', 'image', 'asset1');
     game.preloader.addAsset('mock-url-2', 'image', 'asset2');
@@ -70,14 +64,52 @@ tap.test('Basic Texture Tests', (t) => {
     const allLoaded = game.preloader.assets.every(asset => asset.loaded);
     t.ok(allLoaded, 'all assets are loaded');
 
-    console.log('game.preloader.assets',  game.preloader.assets)
-    let entityData = {
-      texture: 'asset1'
-    };
-    let texture = game.getTexture(entityData.texture);
-    t.ok(texture.url === mockRoot + 'mock-url-1', 'Texture url should match');
-    t.ok(texture.key === 'asset1', 'Texture key should match');
+  });
+
+  t.test('createEntity() function - texture as string', (t) => {
+    const entityId = 1;
+    const entity = game.createEntity({
+      id: entityId,
+      type: 'TEST',
+      texture: 'asset1',
+      width: 33,
+      height: 44,
+      radius: 55,
+      position: { x: 10, y: 20 }
+    });
+
+    t.equal(game.getComponent(entityId, 'position').x, 10);
+    t.equal(game.getComponent(entityId, 'position').y, 20);
+    t.equal(game.getComponent(entityId, 'width'), 33);
+    t.equal(game.getComponent(entityId, 'height'), 44);
+    t.equal(game.getComponent(entityId, 'radius'), 55);
+    t.equal(game.getComponent(entityId, 'type'), 'TEST');
+    t.equal(entity.id, entityId);
+
+    // asset the texture as string value
+    t.equal(game.getComponent(entityId, 'texture'), 'asset1');
+
     t.end();
   });
+
+
+  /*
+  t.test('createEntity() function - texture as sprite and sprite sheet', (t) => {
+    const entity = game.createEntity({
+      id: entityId,
+      type: 'TEST',
+      texture: {
+
+      },
+      width: 33,
+      height: 44,
+      radius: 55,
+      position: { x: 10, y: 20 }
+    });
+
+
+    t.end();
+  });
+  */
 
 });
