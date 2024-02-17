8cac1e5c20ac1254c54115de65ac5521ff617081
Marak <marak.squires@gmail.com>
2024-02-17 03:03:57 -0500

    Ensures that `Plugin.init()` is called before Promise resolves Refactors `game.use()` for clarity

--- mantra-game/lib/Game/use.js
+++ mantra-game/lib/Game/use.js
@@ -1,156 +1,96 @@
 export default function use(game) {
-  return async function use(pluginInstanceOrId, options = {}, cb) {
-
-    if (typeof cb === 'undefined') {
-      cb = function noop() { };
-    }
-
-    // TODO: make game configurable
+  return async function use(pluginInstanceOrId, options = {}, cb = () => {}) {
     let basePath = '/plugins/'; // Base path for loading plugins
     basePath = game.scriptRoot + basePath;
-    //console.log("FOUND SCRIPT ROOT", game.scriptRoot)
-    //console.log("LOADING FROM BASEPATH", basePath)
-    // Check if the argument is a string (plugin ID)
+
     if (typeof pluginInstanceOrId === 'string') {
       const pluginId = pluginInstanceOrId;
-      // Check if the plugin is already loaded or loading
       if (game._plugins[pluginId]) {
-        // maybe add world here?
         console.log(`Plugin ${pluginId} is already loaded or loading.`);
         return game;
       }
 
       if (game.isServer) {
-        // console.log('pluginId', pluginId, game.plugins)
         if (game.plugins[pluginId]) {
-          // console.log('loading plugin', pluginId, game.plugins[pluginId])
           return game.use(new game.plugins[pluginId](options));
         }
-
-        console.log(`Attempted to load plugin by string name "${pluginId}"on server, could not find! skipping`);
+        console.log(`Attempted to load plugin by string name "${pluginId}" on server, could not find! Skipping`);
         return;
       }
 
-      // Mark the plugin as loading
       game._plugins[pluginId] = { status: 'loading' };
       game.loadingPluginsCount++;
       game.emit('plugin::loading', pluginId);
 
-      // Dynamically load the plugin script
-      const scriptUrl = `${basePath}${pluginId}.js`;
-      game.loadPluginScript(scriptUrl).then(async function () {
-        // The script is expected to call `game.use(pluginInstance)` after loading
+      try {
+        const scriptUrl = `${basePath}${pluginId}.js`;
+        await game.loadPluginScript(scriptUrl);
+
         console.log(`Loaded: ${pluginId}`);
 
-        if (typeof PLUGINS === 'object') {
-          //console.log('creating new instance', pluginId, PLUGINS[pluginId], PLUGINS)
+        if (typeof PLUGINS === 'object' && PLUGINS[pluginId]) {
           let pluginInstance = new PLUGINS[pluginId].default(options);
-
-          if (pluginInstance.preload) {
-            // alert('pluginInstance.preload')
-            // console.log("pluginInstance.preload", pluginInstance.preload)
-            // await pluginInstance.preload(game);
-            // we could preload here as well, i don't think we need to since it will happen below
-          }
-
-          game.use(pluginInstance);
-          // check to see if pluginInstance is async, if so
-          // we'll assume it will emit a ready event when it's ready
-          if (pluginInstance.async) { // TODO: remove this and have better async init semantics
-            // plugin must perform async operation before it's ready
-            // plugin author *must* emit their own ready event game will not start
-          } else {
-            game.loadingPluginsCount--;
-            delete game._plugins[pluginId];
-            game.emit('plugin::ready::' + pluginId, pluginInstance);
-            cb();
-          }
+          await handlePluginInstance(game, pluginInstance, pluginId, options, cb);
         } else {
-          // decrement loadingPluginsCount even if it fails
-          // game means applications will attempt to load even if plugins fail
           console.log('Warning: PLUGINS object not found, cannot load plugin', pluginId);
-          delete game._plugins[pluginId];
-          game.loadingPluginsCount--;
-          cb(new Error('PLUGINS object not found, cannot load plugin'));
+          throw new Error('PLUGINS object not found, cannot load plugin');
         }
-      }).catch(function (err) {
+      } catch (err) {
         console.error(`Error loading plugin ${pluginId}:`, err);
         game._plugins[pluginId] = { status: 'error' };
-        throw err;
-      });
-
-      return game;
-    }
-
-    // Handling plugin instances
-    if (typeof pluginInstanceOrId.id === 'undefined') {
-      console.log('Error with pluginInstance', pluginInstanceOrId);
-      throw new Error('All plugins must have a static id property');
-    }
-
-
-    if (typeof pluginInstanceOrId.build === 'function') {
-      extendEntityBuilder(game, pluginInstanceOrId);
-    }
-
-    pluginGameSceneMethods(game, pluginInstanceOrId);
-
-    const pluginId = pluginInstanceOrId.id;
-    game.loadedPlugins.push(pluginId);
-
-    //
-    // If the Plugin has a .preload() method, call it
-    //
-    if (pluginInstanceOrId.preload) {
-      await pluginInstanceOrId.preload(game);
-    }
-
-    pluginInstanceOrId.init(game, game.engine, game.scene);
-    game._plugins[pluginId] = pluginInstanceOrId;
-
-    // can we only check static property here? no need to check instance.type?
-    if (pluginInstanceOrId.type === 'world' || pluginInstanceOrId.constructor.type === 'world') {
-      game.worlds.push(pluginInstanceOrId);
-      // register all worlds as systems ( for now )
-      // we could make this a config flag of the scene ( for performance )
-      game.systemsManager.addSystem(pluginId, pluginInstanceOrId);
-    }
-
-    game.emit(`plugin::loaded::${pluginId}`, pluginInstanceOrId);
-    game.emit('plugin::loaded', pluginId);
-
-    // Remark: Duplicate conditional, remove and cleanup
-    if (typeof pluginInstanceOrId.type !== 'undefined' && pluginInstanceOrId.type === 'world') {
-      game.emit(`world::loaded::${pluginInstanceOrId.id}`, pluginInstanceOrId);
-      game.emit('world::loaded', pluginInstanceOrId);
-    }
-
-    game.data.plugins = game.data.plugins || {};
-    game.data.plugins[pluginId] = options;
-
-    if (pluginInstanceOrId.constructor.type === 'scene') {
-      // Remark: why was mutating game.scenes not working as expected?
-      // game.scenes[pluginId] = pluginInstanceOrId;
-      // data scopes seems OK here
-      game.data.scenes = game.data.scenes || {};
-      game.data.scenes[pluginId] = pluginInstanceOrId;
-
-      // register all scenes as systems ( for now )
-      // we could make this a config flag of the scene ( for performance )
-      game.systemsManager.addSystem(pluginId, pluginInstanceOrId);
+        game.loadingPluginsCount--;
+        cb(err);
+        throw err; // Rethrow or handle as needed
+      }
+    } else {
+      if (!pluginInstanceOrId.id) {
+        console.log('Error with pluginInstance', pluginInstanceOrId);
+        throw new Error('All plugins must have a static id property');
+      }
+      await handlePluginInstance(game, pluginInstanceOrId, pluginInstanceOrId.id, options, cb);
     }
 
     return game;
-
   }
 }
 
-//
-// Extends plugins with scoped scene methods
-//
-function pluginGameSceneMethods (game, pluginInstance) {
+async function handlePluginInstance(game, pluginInstance, pluginId, options, cb) {
+  extendEntityBuilder(game, pluginInstance);
+  pluginGameSceneMethods(game, pluginInstance);
 
-  // attach scene methods to plugin, game methods wrapped to scope entity to plugin scene
+  game.loadedPlugins.push(pluginId);
+
+  if (pluginInstance.preload) {
+    await pluginInstance.preload(game);
+  }
+
+  pluginInstance.init(game, game.engine, game.scene);
+  game._plugins[pluginId] = pluginInstance;
+  game.loadingPluginsCount--;
+  delete game._plugins[pluginId];
+
+  game.emit(`plugin::loaded::${pluginId}`, pluginInstance);
+  game.emit('plugin::loaded', pluginId);
+  cb();
+
+  if (pluginInstance.type === 'world' || pluginInstance.constructor.type === 'world') {
+    game.worlds.push(pluginInstance);
+    game.systemsManager.addSystem(pluginId, pluginInstance);
+    game.emit(`world::loaded::${pluginInstance.id}`, pluginInstance);
+    game.emit('world::loaded', pluginInstance);
+  }
+
+  if (pluginInstance.constructor.type === 'scene') {
+    game.data.scenes = game.data.scenes || {};
+    game.data.scenes[pluginId] = pluginInstance;
+    game.systemsManager.addSystem(pluginId, pluginInstance);
+  }
+
+  game.data.plugins = game.data.plugins || {};
+  game.data.plugins[pluginId] = options;
+}
+
+function pluginGameSceneMethods(game, pluginInstance) {
   pluginInstance.createEntity = function (data) {
     data.scene = pluginInstance.id;
     return game.createEntity(data);
@@ -168,29 +108,15 @@ function pluginGameSceneMethods (game, pluginInstance) {
 
 function extendEntityBuilder(game, pluginInstance) {
   let pluginId = pluginInstance.id;
-
-  // Add a new method to the EntityBuilder class prototype
   game.EntityBuilder.prototype[pluginId] = function(...args) {
-
-    // call the build method with it's own plugin scope + any potential entityData config objects
     const componentValue = pluginInstance.build.call(pluginInstance, ...args);
-
     if (typeof componentValue === 'object') {
-      // assume nested object and iterate
-      // TODO: array
       for (let key in componentValue) {
         this.config[key] = componentValue[key];
       }
-    }
-
-    if (typeof componentValue === 'number' || typeof componentValue === 'string') {
-      // Update the config with the new component
+    } else if (typeof componentValue === 'number' || typeof componentValue === 'string') {
       this.config[pluginId] = componentValue;
     }
-
-    // TODO: function / undefined
-
-    // Return 'this' to maintain the fluent interface
     return this;
   };
 }
